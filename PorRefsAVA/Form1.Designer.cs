12390bc6119d752da036b96c150d7f55152657ee
Dante Souza e Souza <lokinmodar@gmail.com>
2020-05-11 17:58:02 -0300

    Full commit

--- PorRefsAVA/Form1.Designer.cs
+++ PorRefsAVA/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PorRefsAVA
 {
-    partial class Form1
+    partial class FrmRefs
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblVarA = new System.Windows.Forms.Label();
+            this.lblVarB = new System.Windows.Forms.Label();
+            this.lblResA = new System.Windows.Forms.Label();
+            this.lblResB = new System.Windows.Forms.Label();
+            this.btnValor = new System.Windows.Forms.Button();
+            this.btnRef = new System.Windows.Forms.Button();
+            this.txtVarA = new System.Windows.Forms.TextBox();
+            this.txtVarB = new System.Windows.Forms.TextBox();
+            this.txtResA = new System.Windows.Forms.TextBox();
+            this.txtResB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblVarA
+            // 
+            this.lblVarA.AutoSize = true;
+            this.lblVarA.Location = new System.Drawing.Point(13, 24);
+            this.lblVarA.Name = "lblVarA";
+            this.lblVarA.Size = new System.Drawing.Size(55, 13);
+            this.lblVarA.TabIndex = 0;
+            this.lblVarA.Text = "Variável A";
+            // 
+            // lblVarB
+            // 
+            this.lblVarB.AutoSize = true;
+            this.lblVarB.Location = new System.Drawing.Point(13, 54);
+            this.lblVarB.Name = "lblVarB";
+            this.lblVarB.Size = new System.Drawing.Size(55, 13);
+            this.lblVarB.TabIndex = 1;
+            this.lblVarB.Text = "Variável B";
+            // 
+            // lblResA
+            // 
+            this.lblResA.AutoSize = true;
+            this.lblResA.Location = new System.Drawing.Point(13, 140);
+            this.lblResA.Name = "lblResA";
+            this.lblResA.Size = new System.Drawing.Size(55, 13);
+            this.lblResA.TabIndex = 2;
+            this.lblResA.Text = "Variável A";
+            // 
+            // lblResB
+            // 
+            this.lblResB.AutoSize = true;
+            this.lblResB.Location = new System.Drawing.Point(13, 169);
+            this.lblResB.Name = "lblResB";
+            this.lblResB.Size = new System.Drawing.Size(55, 13);
+            this.lblResB.TabIndex = 3;
+            this.lblResB.Text = "Variável B";
+            // 
+            // btnValor
+            // 
+            this.btnValor.Location = new System.Drawing.Point(12, 100);
+            this.btnValor.Name = "btnValor";
+            this.btnValor.Size = new System.Drawing.Size(75, 23);
+            this.btnValor.TabIndex = 4;
+            this.btnValor.Text = "Por Valor";
+            this.btnValor.UseVisualStyleBackColor = true;
+            this.btnValor.Click += new System.EventHandler(this.btnValor_Click);
+            // 
+            // btnRef
+            // 
+            this.btnRef.Location = new System.Drawing.Point(109, 100);
+            this.btnRef.Name = "btnRef";
+            this.btnRef.Size = new System.Drawing.Size(75, 23);
+            this.btnRef.TabIndex = 5;
+            this.btnRef.Text = "Por Ref";
+            this.btnRef.UseVisualStyleBackColor = true;
+            this.btnRef.Click += new System.EventHandler(this.btnRef_Click);
+            // 
+            // txtVarA
+            // 
+            this.txtVarA.Location = new System.Drawing.Point(84, 21);
+            this.txtVarA.Name = "txtVarA";
+            this.txtVarA.Size = new System.Drawing.Size(100, 20);
+            this.txtVarA.TabIndex = 6;
+            // 
+            // txtVarB
+            // 
+            this.txtVarB.Location = new System.Drawing.Point(84, 54);
+            this.txtVarB.Name = "txtVarB";
+            this.txtVarB.Size = new System.Drawing.Size(100, 20);
+            this.txtVarB.TabIndex = 7;
+            // 
+            // txtResA
+            // 
+            this.txtResA.Location = new System.Drawing.Point(84, 137);
+            this.txtResA.Name = "txtResA";
+            this.txtResA.Size = new System.Drawing.Size(100, 20);
+            this.txtResA.TabIndex = 8;
+            // 
+            // txtResB
+            // 
+            this.txtResB.Location = new System.Drawing.Point(84, 166);
+            this.txtResB.Name = "txtResB";
+            this.txtResB.Size = new System.Drawing.Size(100, 20);
+            this.txtResB.TabIndex = 9;
+            // 
+            // FrmRefs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(202, 204);
+            this.Controls.Add(this.txtResB);
+            this.Controls.Add(this.txtResA);
+            this.Controls.Add(this.txtVarB);
+            this.Controls.Add(this.txtVarA);
+            this.Controls.Add(this.btnRef);
+            this.Controls.Add(this.btnValor);
+            this.Controls.Add(this.lblResB);
+            this.Controls.Add(this.lblResA);
+            this.Controls.Add(this.lblVarB);
+            this.Controls.Add(this.lblVarA);
+            this.Name = "FrmRefs";
+            this.Text = "Valor / Referência";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblVarA;
+        private System.Windows.Forms.Label lblVarB;
+        private System.Windows.Forms.Label lblResA;
+        private System.Windows.Forms.Label lblResB;
+        private System.Windows.Forms.Button btnValor;
+        private System.Windows.Forms.Button btnRef;
+        private System.Windows.Forms.TextBox txtVarA;
+        private System.Windows.Forms.TextBox txtVarB;
+        private System.Windows.Forms.TextBox txtResA;
+        private System.Windows.Forms.TextBox txtResB;
     }
 }
 
--- PorRefsAVA/Form1.cs
+++ PorRefsAVA/Form1.cs
@@ -1,20 +1,78 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PorRefsAVA
 {
-    public partial class Form1 : Form
+    public partial class FrmRefs : Form
     {
-        public Form1()
+        public FrmRefs()
         {
             InitializeComponent();
+        }
+
+        private static void PorValor(double varA, double varB)
+        {
+            //altera o valor das variáveis locais que são passadas por valor
+            //não altera os valores das variáveis originais (declaradas no botão Por Valor
+            //essas alterações são válidas somente dentro deste escopo
+            varA = varA * 2;
+            varB = varB * 5;
+        }
+
+        private static void PorReferencia(ref double varA, ref double varB)
+        {
+            //altera o valor das variáveis que são passadas por referência
+            //altera os valores das variáveis originais (declaradas no botão Por Referencia
+            varA = varA * 2;
+            varB = varB * 5;
+        }
+
+        private void LimparControles()
+        {
+            //limpa os textos dos controles
+            txtResA.Clear();
+            txtResB.Clear();
+        }
+
+        private void btnValor_Click(object sender, EventArgs e)
+        {
+            //declaração das variáveis original
+
+            //entrada de dados
+            var varA = Convert.ToDouble(txtVarA.Text);
+            var varB = Convert.ToDouble(txtVarB.Text);
+
+            //chama o procedimento porValor passando os parâmetros por por valor,
+            //ou seja, é passada uma cópia das variáveis
+            PorValor(varA, varB);
+
+            //chama o procedimento para Limpar os controles
+            LimparControles();
+
+            //saída de dados para verificar que não houve alteração no valor da variável original
+            // os valores das variáveis não foram alterados no procedimento
+            txtResA.Text = varA.ToString();
+            txtResB.Text = varB.ToString();
+        }
+
+        private void btnRef_Click(object sender, EventArgs e)
+        {
+            //declaração das variáveis originais
+
+            //entrada de dados
+            var varA = Convert.ToDouble(txtVarA.Text);
+            var varB = Convert.ToDouble(txtVarB.Text);
+
+            //chama o procedimento porReferencia passando os parâmetros por referência
+            PorReferencia(ref varA, ref varB);
+
+            //chama o procedimento para Limpar os controles
+            LimparControles();
+
+            //saída de dados para verificar a alteração no valor da variável original
+            // os valores das variáveis foram alterados no procedimento
+            txtResA.Text = varA.ToString();
+            txtResB.Text = varB.ToString();
         }
     }
 }
